ab92e63e59e110e3ab22dc79d2cc88d087ca4365
kgbandula <kgbandula@gmail.com>
2023-02-26 00:20:55 +1300

    login test added

---
+++ TA2023/Program.cs
@@ -0,0 +1,35 @@
+﻿//Open the ChromeBrowser
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+//Launch TurnUp portal
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+//Identify username textbox and enter valid username
+IWebElement usernameTextBox = driver.FindElement(By.Id("UserName"));
+usernameTextBox.SendKeys("hari");
+
+//Identify password textbox and enter valid password
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+Thread.Sleep(1000);
+
+//Identify login button and click on it
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+
+//Check if user has successfully logged in
+IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+
+if (hellohari.Text == "Hello hari!")
+{
+    Console.WriteLine("Logged in successfully");
+}
+else
+{ 
+    Console.WriteLine("Login failed");
+}
